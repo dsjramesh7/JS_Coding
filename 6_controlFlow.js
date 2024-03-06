d7c09103ed03649795c7e87e919660423061aa5d
dsjramesh7 <rameshdussa07@gmail.com>
2024-03-06 17:22:47 +0530

    practice

---
+++ 6_controlFlow.js
@@ -0,0 +1,17 @@
+// Control Flow Statements
+const alphabets = ["a", "b", "c", "d", "e"];
+
+// For loops
+for (i = 0; i < alphabets.length; i++) {
+  console.log(`each alphabet is ${alphabets[i]} at index ${i}`);
+}
+
+// while loops
+j = 0;
+while (j < alphabets.length) {
+  console.log(`alphabet is ${alphabets[j]}`);
+  j++; // if you increment or decrement conditional your pc will get hang lol
+}
+
+
+// we have do while but its boring
