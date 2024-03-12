7bf55a6a7ac98a0973dd0aa02e25550fcd5a5ade
dsjramesh7 <rameshdussa07@gmail.com>
2024-03-13 03:21:33 +0530

    practice

---
+++ 24_DateTime.js
@@ -0,0 +1,4 @@
+let currentDate = new Date(0);
+console.log(currentDate);
+
+// you can modify according to yourself
---
+++ 25_localStorage.js
@@ -0,0 +1,13 @@
+// console.log(localStorage);
+
+//set Item
+localStorage.setItem("skills", "coding");
+localStorage.setItem("age", "21");
+console.log(localStorage);
+
+///get Item
+console.log(localStorage.getItem("age"));
+
+// remove Item
+console.log(localStorage.removeItem("age"));
+console.log(localStorage);
